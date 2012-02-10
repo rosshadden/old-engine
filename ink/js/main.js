cfedcb514686d81e4209aaf216a5a21150aa8c3c
Ross Hadden <rosshadden@gmail.com>
2012-02-10 09:59:16 -0500

    I don't remember.

--- ink/js/main.js
+++ ink/js/main.js
@@ -2,6 +2,9 @@ require(['jquery','engine/engine'],function($,Engine){
 	var
 		game = new Engine({
 			screen:	$('#screen')[0],
+			
+			map:	'map1',
+			
 			init: function(){
 				var self = this;
 			},
